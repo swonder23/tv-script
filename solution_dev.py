dbc914ae82be671ec83a512be2ef59aea786db35
Steven Côté <scote01@hotmail.com>
2020-08-22 22:38:54 -0400

    Development progress + saved pre-processed tv script data

--- solution_dev.py
+++ solution_dev.py
@@ -49,10 +49,15 @@ import problem_unittests as tests
 
 # create a list of words
 words = text.split()
+words[:50]
 words.index("jerry:")
 # /n is not kept when splitting with split function. When running, token_lookup() will be ran first, replacing /n by ||Return||, it's how we will capture
 # the line breaks
 # ALSO, why do we need "text = text[81:] in the helper file?" put that in comments?
+
+
+from collections import Counter
+
 
 def create_lookup_tables(text):
     """
@@ -60,13 +65,134 @@ def create_lookup_tables(text):
     :param text: The text of tv scripts split into words
     :return: A tuple of dicts (vocab_to_int, int_to_vocab)
     """
-    # TODO: Implement Function
-    
-    # return tuple
-    return (None, None)
+    word_counts = Counter(text)
+    # sorting the words from most to least frequent in text occurrence
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    # create int_to_vocab dictionaries
+    int_to_vocab = {ii: word for ii, word in enumerate(sorted_vocab)}
+    vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
+    dict_tuple = (vocab_to_int, int_to_vocab)
 
+    # return vocab_to_int, int_to_vocab
+    return dict_tuple
 
-"""
-DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
-"""
+# vocab_to_int, int_to_vocab = create_lookup_tables(words)
+dict_tuple = create_lookup_tables(words)
 tests.test_create_lookup_tables(create_lookup_tables)
+
+
+
+def token_lookup():
+    """
+    Generate a dict to turn punctuation into a token.
+    :return: Tokenized dictionary where the key is the punctuation and the value is the token
+    """
+    
+    tokendict = {
+      '.': '||Period||',
+      ',': '||Comma||',
+      '"': '||Quotation_mark||',
+      ';': '||Semicolon||',
+      '!': '||Exclamation_mark||',
+      '?': '||Question_mark||',
+      '(': '||Left_Parenthese||',
+      ')': '||Right_Parenthese||',
+      '-': '||Dash||',
+      '\n': '||Return||'
+    }
+        
+    return tokendict
+
+token_dict = token_lookup()
+tests.test_tokenize(token_lookup)
+
+
+
+##########################################
+## PRE-PROCESS ALL THE DATA AND SAVE IT ##  
+##########################################
+
+# pre-process training data
+helper.preprocess_and_save_data(data_dir, token_lookup, create_lookup_tables)
+
+
+
+################
+## CHECKPOINT ##  
+################
+
+import helper
+import problem_unittests as tests
+
+int_text, vocab_to_int, int_to_vocab, token_dict = helper.load_preprocess()
+
+
+
+####################
+## INPUT BATCHING ##  
+####################
+
+import torch
+
+# Check for a GPU
+train_on_gpu = torch.cuda.is_available()
+if not train_on_gpu:
+    print('No GPU found. Please use a GPU to train your neural network.')
+    
+    
+    
+from torch.utils.data import TensorDataset, DataLoader
+
+
+def batch_data(words, sequence_length, batch_size):
+    """
+    Batch the neural network data using DataLoader
+    :param words: The word ids of the TV scripts
+    :param sequence_length: The sequence length of each batch
+    :param batch_size: The size of each batch; the number of sequences in a batch
+    :return: DataLoader with batched data
+    """
+    
+    # return a dataloader
+    return None
+
+# there is no test for this function, but you are encouraged to create
+# print statements and tests of your own
+
+
+
+
+words = int_text
+sequence_length = 5 ## learn 5 words to generate the 6th
+batch_size = 10 ## rnn will be trained iteratively with batches of 10 random mini-batches of words sequences of length 5 
+
+    batch_size_total = batch_size * sequence_length
+    # total number of batches we can make
+    n_batches = len(words)//batch_size_total
+    
+    # Keep only enough words to make full batches
+    words = words[:n_batches * batch_size_total]
+    # Reshape into batch_size rows
+    words = np.asarray(words)
+    words.shape
+    words = words.reshape((-1, batch_size))
+ 
+###########################   
+## THIS DOESN'T WORK YET ## 
+    # iterate through the array, one sequence at a time
+    # for n in range(0, words.shape[0], sequence_length):
+    for n in range(0, 20, sequence_length):
+        # The features
+        x = words[:, n:n+sequence_length]
+        # The targets, shifted by one
+        y = np.zeros_like(x)
+        try:
+            y[:, :-1], y[:, -1] = x[:, 1:], words[:, n+sequence_length]
+        except IndexError:
+            y[:, :-1], y[:, -1] = x[:, 1:], words[:, 0]
+        yield x, y
+
+
+
+
+
