7904a107c842de71068efe427379cabe1266b1b5
Steven Côté <scote01@hotmail.com>
2020-08-25 15:43:00 -0400

    Development progress: RNN class defined + forward backprop completed

--- solution_dev.py
+++ solution_dev.py
@@ -307,3 +307,147 @@ print(sample_x)
 
 print(sample_y.shape)
 print(sample_y)
+
+
+
+
+##############################
+## BUILD THE NEURAL NETWORK ##  
+##############################
+
+import torch.nn as nn
+
+class RNN(nn.Module):
+    
+    def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, dropout=0.5):
+        """
+        Initialize the PyTorch RNN Module
+        :param vocab_size: The number of input dimensions of the neural network (the size of the vocabulary)
+        :param output_size: The number of output dimensions of the neural network
+        :param embedding_dim: The size of embeddings, should you choose to use them        
+        :param hidden_dim: The size of the hidden layer outputs
+        :param dropout: dropout to add in between LSTM/GRU layers
+        """
+        super(RNN, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.output_size = output_size
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+     
+        # embedding and LSTM layers
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, 
+                            dropout=dropout, batch_first=True)
+        
+        # dropout layer
+        self.dropout = nn.Dropout(dropout)
+        
+        ## define the final, fully-connected output layer
+        self.fc = nn.Linear(hidden_dim, output_size)
+    
+    
+    def forward(self, nn_input, hidden):
+        """
+        Forward propagation of the neural network
+        :param nn_input: The input to the neural network
+        :param hidden: The hidden state        
+        :return: Two Tensors, the output of the neural network and the latest hidden state
+        """
+
+        batch_size = nn_input.size(0)
+        
+        # embeddings and lstm_out
+        nn_input = nn_input.long()  
+        embeds = self.embedding(nn_input)
+        lstm_out, hidden = self.lstm(embeds, hidden)
+    
+        # stack up lstm outputs
+        lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+        
+        # dropout and fully-connected layer
+        out = self.dropout(lstm_out)
+        out = self.fc(out)
+        
+        # reshape into (batch_size, seq_length, output_size)
+        out = out.view(batch_size, -1, self.output_size)
+        # get last batch
+        out = out[:, -1]  
+
+        # return one batch of output word scores and the hidden state
+        return out, hidden
+    
+    
+    def init_hidden(self, batch_size):
+        '''
+        Initialize the hidden state of an LSTM/GRU
+        :param batch_size: The batch_size of the hidden state
+        :return: hidden state of dims (n_layers, batch_size, hidden_dim)
+        '''
+        
+        # initialize hidden state with zero weights, and move to GPU if available
+
+        weight = next(self.parameters()).data
+        
+        if (train_on_gpu):
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+                  weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+        else:
+            hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                      weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+        
+        return hidden
+
+
+tests.test_rnn(RNN, train_on_gpu)
+
+
+
+
+
+def forward_back_prop(rnn, optimizer, criterion, inp, target, hidden):
+    """
+    Forward and backward propagation on the neural network
+    :param decoder: The PyTorch Module that holds the neural network
+    :param decoder_optimizer: The PyTorch optimizer for the neural network
+    :param criterion: The PyTorch loss function
+    :param inp: A batch of input to the neural network
+    :param target: The target output for the batch of input
+    :return: The loss and the latest hidden state Tensor
+    """
+    
+    # move data to GPU, if available
+    if(train_on_gpu):
+        inp, target = inp.cuda(), target.cuda()
+    
+    # perform backpropagation and optimization
+    # Creating new variables for the hidden state, otherwise
+    # we'd backprop through the entire training history
+    hidden = tuple([each.data for each in hidden])
+    
+    # zero accumulated gradients
+    rnn.zero_grad()
+    
+    # get the output from the model
+    output, h = rnn(inp, hidden)
+    
+    # calculate the loss and perform backprop
+    loss = criterion(output.squeeze(), target.long())
+    loss.backward()
+    # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+    clip=5 # gradient clipping
+    nn.utils.clip_grad_norm_(rnn.parameters(), clip)
+    optimizer.step()
+    loss_value = loss.item()
+
+    # return the loss over a batch and the hidden state produced by our model
+    return loss_value, h
+
+
+tests.test_forward_back_prop(RNN, forward_back_prop, train_on_gpu)
+
+
+
